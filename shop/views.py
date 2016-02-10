7affaa2ff14b9141ef661f59fafdb4163bdd1d38
maratkanov-a <a.s.maratkanov@gmail.com>
2016-02-10 09:45:21 +0300

    added fontawesome, also added product and category pages

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'home.html'
+
+
+class CategoryPage(TemplateView):
+    template_name = 'category.html'
+
+
+class ProductsWithCategoryPage(TemplateView):
+    template_name = 'products_with_category.html'
